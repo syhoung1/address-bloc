5df29c5429aca2f1bb2d4db67e5d1236c9a978fc
Steven <syhoung1@gmail.com>
2017-03-23 20:03:00 +0000

    more menu options

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -54,9 +54,6 @@ class MenuController
         puts "End of entries"
     end
 
-    def search_entries
-    end
-
     def create_entry
         system "clear"
         puts "New AddressBloc Entry"
@@ -74,6 +71,23 @@ class MenuController
     end
 
     def read_csv
+        print "Enter CSV file to import: "
+        file_name = gets.chomp
+        
+        if file_name.empty?
+            system "clear"
+            puts "No CSV file read"
+            main_menu
+        end
+
+        begin
+            entry_count = address_book.import_from_csv(file_name).count
+            system "clear"
+            puts "#{entry_count} new entries added from #{file_name}"
+        rescue
+            puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+            read_csv
+        end
     end
 
     def entry_submenu(entry)
@@ -87,7 +101,10 @@ class MenuController
         case selection
             when "n"
             when "d"
+                delete_entry(entry)
             when "e"
+                edit(entry)
+                entry_submenu(entry)
             when "m"
                 system "clear"
                 main_menu
@@ -95,6 +112,69 @@ class MenuController
                 system "clear"
                 puts "#{selection} is not valid input"
                 entry_submenu(entry)
+        end
+    end
+
+    def delete_entry(entry)
+        address_book.entries.delete(entry)
+        puts "#{entry.name} has been deleted"
+    end
+
+    def edit(entry)
+        print "Update name: "
+        name = gets.chomp
+        print "Update phone number: "
+        phone_number = gets.chomp
+        print "Update email: "
+        email = gets.chomp
+
+        entry.name = name if !name.empty?
+        entry.phone_number = phone_number if !phone_number.empty?
+        entry.email = email if !email.empty?
+        system "clear"
+        
+        puts "Updated entry: "
+        puts entry
+    end
+
+    def search_entries
+        print "Search by name: "
+        name = gets.chomp
+
+        match = address_book.binary_search(name)
+        system "clear"
+        
+        if match
+            puts match.to_s
+            search_submenu(match)
+        else
+            puts "No entry found for #{name}"
+        end
+    end
+
+    def search_submenu(entry)
+        puts "\nd - delete entry"
+        puts "e - edit entry"
+        puts "m - return to main menu"
+        selection = gets.chomp
+
+        case selection
+            when "d"
+                system "clear"
+                delete_entry(entry)
+                main_menu
+            when "e"
+                edit(entry)
+                system "clear"
+                main_menu
+            when "m"
+                system "clear"
+                main_menu
+            else
+                system "clear"
+                puts "#{selection} is not a valid input"
+                puts entry.to_s
+                search_submenu(entry)
         end
     end
 end
